d7cf9f1a6f83143dc82238e05ef0834dba1311c6
vpcreatz <119391455+vpcreatz@users.noreply.github.com>
2023-01-16 19:25:49 +0530

    Create start.sh

---
+++ start.sh
@@ -0,0 +1,12 @@
+if [ -z $UPSTREAM_REPO ]
+then
+  echo "Cloning main Repository"
+  git clone https://github.com/vpcreatz/Media-Search-bot.git /Media-Search-bot
+else
+  echo "Cloning Custom Repo from $UPSTREAM_REPO "
+  git clone $UPSTREAM_REPO /Elsa
+fi
+cd /Elsa
+pip3 install -U -r requirements.txt
+echo "𝑺𝑻𝑨𝑹𝑻𝑰𝑵𝑮 𝑴𝑬𝑫𝑰𝑨-𝑺𝑬𝑨𝑹𝑪𝑯-𝑩𝑶𝑻....🔍"
+python3 bot.py
