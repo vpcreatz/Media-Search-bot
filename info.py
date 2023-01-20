b2001d95ffe03b12c4b8656bd7e32408e23aabd7
vpcreatz <119391455+vpcreatz@users.noreply.github.com>
2023-01-20 22:21:19 +0530

    Update info.py

--- info.py
+++ info.py
@@ -7,7 +7,7 @@ id_pattern = re.compile(r'^.\d+$')
 SESSION = environ.get('SESSION', 'Media_search')
 USER_SESSION = environ.get('USER_SESSION', 'User_Bot')
 API_ID = int(environ.get('API_ID', '20960397'))
-API_HASH = environ['d68d847d3abb2087bf74f5d0683c2993']
+API_HASH = environ.get('API_HASH', 'd68d847d3abb2087bf74f5d0683c2993')
 BOT_TOKEN = environ.get('BOT_TOKEN', '5830715055:AAFSgnqRgVcP-gHXUlJMn9EtwmNZrqNvKrk')
 USERBOT_STRING_SESSION = environ.get('USERBOT_STRING_SESSION')
 
@@ -16,7 +16,7 @@ CACHE_TIME = int(environ.get('CACHE_TIME', 300))
 USE_CAPTION_FILTER = bool(environ.get('USE_CAPTION_FILTER', False))
 
 # Admins, Channels & Users
-ADMINS = [int(admin) if id_pattern.search(admin) else admin for admin in environ['1327973537'].split()]
+ADMINS = [int(admin) if id_pattern.search(admin) else admin for admin in environ('1327973537').split()]
 CHANNELS = [int(ch) if id_pattern.search(ch) else ch for ch in environ['-1001798480646'].split()]
 auth_users = [int(user) if id_pattern.search(user) else user for user in environ.get('AUTH_USERS', '1327973537').split()]
 AUTH_USERS = (auth_users + ADMINS) if auth_users else []
@@ -24,8 +24,8 @@ auth_channel = environ.get('-1001798480646')
 AUTH_CHANNEL = int(auth_channel) if auth_channel and id_pattern.search(auth_channel) else auth_channel
 
 # MongoDB information
-DATABASE_URI = environ['mongodb+srv://vpcreatz:VPCREATION@cluster0.6lncjwt.mongodb.net/?retryWrites=true&w=majority']
-DATABASE_NAME = environ['Cluster0']
+DATABASE_URI = environ.get('mongodb+srv://vpcreatz:VPCREATION@cluster0.6lncjwt.mongodb.net/?retryWrites=true&w=majority')
+DATABASE_NAME = environ.get('Cluster0')
 COLLECTION_NAME = environ.get('COLLECTION_NAME', 'Telegram_files')
 
 # Messages
