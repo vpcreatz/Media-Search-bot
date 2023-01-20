43e829c586c0d39980eee3aafc6f6b424416c7d2
vpcreatz <119391455+vpcreatz@users.noreply.github.com>
2023-01-20 22:35:05 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -2,7 +2,6 @@ import logging, os, math
 import logging.config
 from pyrogram import Client, __version__
 from pyrogram.raw.all import layer
-from utils import Media
 from info import SESSION, API_ID, API_HASH, BOT_TOKEN
 
 # Get logging configurations
