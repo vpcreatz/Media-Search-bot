534d87f3ab519749997019fa5129a6846cb8a228
vpcreatz <119391455+vpcreatz@users.noreply.github.com>
2023-01-20 22:32:11 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -3,7 +3,6 @@ import logging.config
 from pyrogram import Client, __version__
 from pyrogram.raw.all import layer
 from info import SESSION, API_ID, API_HASH, BOT_TOKEN
-from utils import temp
 from typing import Union, Optional, AsyncGenerator
 from pyrogram import types
 from datetime import datetime
